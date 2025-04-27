9ff82615ee939ce8bf2cc9ddb30f9b30c3f97686
kc <kokchoong611@gmail.com>
2025-04-27 18:15:13 +0800

    feat; tidy header footer

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function openNewTab(url) {
   window.open(url, '_blank');
 }
+
+function scrollToTop() {
+  window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // Smooth scrolling effect
+  });
+}
