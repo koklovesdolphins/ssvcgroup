95512f3f005efce213b8d8d07ee5377d56a227fb
kc <kokchoong611@gmail.com>
2025-04-27 21:07:29 +0800

    feat: dianomatic page

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ function scrollToTop() {
       behavior: 'smooth' // Smooth scrolling effect
   });
 }
+
+function loadHeaderFooter() {
+  const timestamp = new Date().getTime(); // Unique timestamp to prevent caching
+
+  fetch(`/header.html?cache-bust=${timestamp}`)
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading header:', error));
+
+  fetch(`/footer.html?cache-bust=${timestamp}`)
+    .then(response => response.text())
+    .then(data => {
+      document.querySelector('footer').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading footer:', error));
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual'; // Disable browser's scroll restoration
+  }
+  window.scrollTo(0, 0); // Ensure the page starts at the top
+  loadHeaderFooter();
+});
